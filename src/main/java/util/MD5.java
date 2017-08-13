18e1b06393b16d17f8bde6115e8060ace12a4ce2
cellargalaxy <cellargalaxy@gmail.com>
2017-08-13 23:44:33 +0800

    给各个楼栋排一下序

---
+++ src/main/java/util/MD5.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by 孵化种子 on 2017/8/6.
+ */
+public class MD5 {
+    private static MessageDigest md5;
+
+    static {
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String encryption(String string) {
+        md5.update(string.getBytes());
+        return new BigInteger(1, md5.digest()).toString(16);
+    }
+}
---
+++ src/main/java/wechatWeb/JsonHostServlet.java
@@ -0,0 +1,33 @@
+package wechatWeb;
+
+
+import org.json.JSONArray;
+import wechat.JsonHost;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by 孵化种子 on 2017/8/6.
+ */
+public class JsonHostServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String token=req.getParameter("token");
+        JSONArray jsonArray=null;
+        if (token!=null && token.equals(JsonHost.jsonHostToken())) {
+            jsonArray=JsonHost.jsonHosts();
+        }else {
+            jsonArray=new JSONArray();
+        }
+
+        resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        out.println(jsonArray);
+        out.close();
+    }
+}
